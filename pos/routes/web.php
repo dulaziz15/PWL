11597ab63bb5be03f760b4d06888ab535d9be958
ABDUL AZIZ <107160974+dulaziz15@users.noreply.github.com>
2025-03-13 23:58:53 +0700

    praktikum 5 - Implementasi Query Builder - migration

--- pos/routes/web.php
+++ pos/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\ProductController;
@@ -32,3 +33,5 @@ Route::get('user/{id}/name/{name}', [UserController::class, 'index']);
 Route::get('penjualan', [PenjualanController::class, 'index']);
 
 Route::get('/level', [LevelController::class, 'index']);
+
+Route::get('/kategori', [KategoriController::class, 'index']);
