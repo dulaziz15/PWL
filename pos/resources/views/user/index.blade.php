b6f38d0f057a8ab3644deabd8d588e20905d8fe6
ABDUL AZIZ <107160974+dulaziz15@users.noreply.github.com>
2025-03-15 14:15:22 +0700

    praktikum 2.4 - Retreiving or Creating Models - Eloquent ORM

--- pos/resources/views/user/index.blade.php
+++ pos/resources/views/user/index.blade.php
@@ -9,14 +9,6 @@
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                <th>jumlah Pengguna</th>
-            </tr>
-            <tr>
-                <td>{{ $data }}</td>
-            </tr>
-        </table>
-        {{-- <table border="1" cellpadding="2" cellspacing="0">
-            <tr>
                 <th>ID</th>
                 <th>Username</th>
                 <th>Nama</th>
@@ -28,7 +20,7 @@
                     <td>{{ $data->nama }}</td>
                     <td>{{ $data->level_id }}</td>
                 </tr>
-        </table> --}}
+        </table>
     </body>
 
     </html>
