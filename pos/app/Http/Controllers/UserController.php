5e90ad4d4925702d8f6910b6aa8598a546ba68d1
ABDUL AZIZ <107160974+dulaziz15@users.noreply.github.com>
2025-03-17 20:44:29 +0700

    praktikum 4 - Implementasi Filtering Datatables - Blade, View, Web Templating (Admin LTE), datatables

--- pos/app/Http/Controllers/UserController.php
+++ pos/app/Http/Controllers/UserController.php
@@ -24,15 +24,19 @@ class UserController extends Controller
 
         $acticeMenu = 'user';
 
-        $this->list();
+        $level = LevelModel::all();
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $acticeMenu]);
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $acticeMenu]);
     }
 
-    public function list()
+    public function list(Request $request)
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
+
+        if($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
 
         return DataTables::of($users)
             ->addIndexColumn() // Menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
