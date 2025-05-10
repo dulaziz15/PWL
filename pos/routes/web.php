d3e13842829b614659cf52712e41ea9301a1528b
ABDUL AZIZ <107160974+dulaziz15@users.noreply.github.com>
2025-05-10 13:08:57 +0700

    P8 - Praktikum dan Tugas 2 - Implementasi Export PDF pada Laravel

--- pos/routes/web.php
+++ pos/routes/web.php
@@ -54,6 +54,7 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/import', [UserController::class, 'import']);
         Route::post('/import_ajax', [UserController::class, 'import_ajax']);
         Route::get('/export_excel', [UserController::class, 'export_excel']);
+        Route::get('/export_pdf', [UserController::class, 'export_pdf']);
     });
 
     Route::middleware(['authorize:ADM'])->group(function () {
@@ -76,6 +77,7 @@ Route::middleware(['auth'])->group(function () {
             Route::get('/import', [LevelController::class, 'import']);
             Route::post('/import_ajax', [LevelController::class, 'import_ajax']);
             Route::get('/export_excel', [LevelController::class, 'export_excel']);
+            Route::get('/export_pdf', [LevelController::class, 'export_pdf']);
         });
     });
 
@@ -98,6 +100,7 @@ Route::middleware(['auth'])->group(function () {
             Route::post('/import_ajax', [BarangController::class, 'import_ajax']);
             Route::delete('{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
             Route::get('/export_excel', [BarangController::class, 'export_excel']);
+            Route::get('/export_pdf', [BarangController::class, 'export_pdf']);
             Route::delete('/{id}', [BarangController::class, 'destroy']);
         });
     });
@@ -121,6 +124,7 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/import', [KategoriController::class, 'import']);
         Route::post('/import_ajax', [KategoriController::class, 'import_ajax']);
         Route::get('/export_excel', [KategoriController::class, 'export_excel']);
+        Route::get('/export_pdf', [KategoriController::class, 'export_pdf']);
     });
 
     Route::group(['prefix' => 'supplier'], function () {
@@ -142,5 +146,6 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/import', [SupplierController::class, 'import']);
         Route::post('/import_ajax', [SupplierController::class, 'import_ajax']);
         Route::get('/export_excel', [SupplierController::class, 'export_excel']);
+        Route::get('/export_pdf', [SupplierController::class, 'export_pdf']);
     });
 });
